63ea2596dde010eb9f1328e113f73b307f43e93a
chriskimty <christaeyoung.kim@gmail.com>
2023-07-18 23:11:39 -0400

    reconfig tailwind with input and output, add fonts, styled header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <section>
+    <section className="flex justify-between items-center wrapper py-5">
       <div>
-        <Link href="/">
+        <Link href="/" className="flex items-center gap-4">
           <Image
             src="/tech_tank_fish_white.png"
             alt="tech tech fish logo"
             width={50}
             height={50}
           />
-          <p>TechTank</p>
+          <p className="text-xl">TechTank</p>
         </Link>
       </div>
       <div>
         <Link href="/code-of-conduct">
-          <div>
+          <div className="btn">
             <p>Code of Conduct</p>
           </div>
         </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient bg-cover">
+    <main className="min-h-screen">
       <Header />
       <h1>TechTank</h1>
       <h2>Your Techie Community</h2>
