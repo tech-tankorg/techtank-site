d724b0f5b49fe49cb282bd71bffb6d9817e356b0
chriskimty <christaeyoung.kim@gmail.com>
2023-07-24 22:46:13 -0400

    moved CoC contents into constants and mapped through contents

--- app/code-of-conduct/page.tsx
+++ app/code-of-conduct/page.tsx
@@ -1,114 +1,49 @@
+import { CoC_Contents } from "@/utils/constants";
+
 export default function CoC() {
-  // TODO: to improve readability, create lists for the texts in a utils/constants file and map through it instead
   return (
     <section>
       <h2>Code of Conduct</h2>
       {/* SCOPE */}
       <div>
         <h4>Scope</h4>
-        <p>
-          We, the admin team at TechTank, are happy to have grown to such a
-          large group in such a short amount of time. As we value the
-          participation and enjoyment of everyone in our community, we have
-          decided to release community guidelines/codes of conduct. Our goal and
-          wish is that everyone has a positive experience, and these codes apply
-          to ALL community spaces: our slack workspace, in-person and virtual
-          social gatherings, and direct messages. Repeated unacceptable
-          behaviour will lead to expulsion from the group as we are here to
-          create a safe and inclusive space for everyone.
-        </p>
+        <p>{CoC_Contents.SCOPE_TEXT}</p>
       </div>
       {/* ENFORCEMENT */}
       <div>
         <h4>Enforcement</h4>
-        <p>
-          Instances of abuse, harassment or any unacceptable behavior can be
-          reported to Chris, Erik, Nonso, and Sammy. We will take corrective
-          action in response to any behavior deemed inappropriate, threatening,
-          offensive or harmful. We have a right to protect this space and
-          individuals within.
-        </p>
+        <p>{CoC_Contents.ENFORCEMENT_TEXT}</p>
         <ul>
-          <li>
-            1. Notice of Correction → We will contact said individual to correct
-            their behavior via slack
-          </li>
-          <li>
-            2. Temporary Ban → banned from the community for weeks. This
-            includes interaction in community spaces, external channels and
-            direct messages on and off the community.
-          </li>
-          <li>3. Permanent Ban</li>
+          {CoC_Contents.ENFORCEMENT_LIST.map((list) => {
+            return (
+              <li key={list.id}>
+                <span>
+                  {list.id}. {list.title}:
+                </span>{" "}
+                {list.text}
+              </li>
+            );
+          })}
         </ul>
       </div>
       {/* UNACCEPTABLE BEHAVIOR */}
       <div>
         <h4>Unacceptable Behavior</h4>
         <ul>
-          <li>
-            A. No hate speech or discriminatory language: Do not engage in or
-            tolerate any form of hate speech, including but not limited to
-            racist, sexist, homophobic, transphobic, or ableist language.
-          </li>
-          <li>
-            B. No discrimination: Treat all individuals with fairness, respect,
-            and dignity, regardless of their race, gender, sexual orientation,
-            religion, nationality, disability, or any other characteristic.
-          </li>
-          <li>
-            C. No harassment: Do not engage in any form of harassment, including
-            verbal, written, physical, or visual harassment. This includes but
-            is not limited to offensive jokes, derogatory comments, unwelcome
-            advances, or persistent unwelcome communication. This includes
-            asking for referrals from people you have not had contact with
-            prior.
-          </li>
-          <li>
-            D. No bullying or intimidation: Do not engage in any behavior
-            intended to intimidate, belittle, or demean others within the group.
-          </li>
-          <li>
-            E. *No feigning surprise: Don&apos;t act surprised or belittle
-            others when they express not knowing something, whether it&apos;s a
-            technical or non-technical topic.
-          </li>
-          <li>
-            F. *No well-actually&apos;s: Avoid making minor corrections solely
-            for the purpose of grandstanding, particularly if they don&apos;t
-            significantly contribute to the conversation.
-          </li>
-          <li>
-            G. *No back-seat driving (in irl, face-to-face contexts): Refrain
-            from intermittently offering unsolicited advice or interrupting
-            ongoing conversations, as it can be disruptive and disrespectful.
-            Instead, fully engage and participate when helping or joining
-            discussions.
-          </li>
-          <li>H. Respect other people’s boundaries. No means no.</li>
-          <li>
-            G. Respect other people’s time. Many folx in the group, including
-            the admins, have personal and professional lives outside of the
-            group. Expect delays in responses especially on weekends and
-            weeknights, and understand it might take time to respond to your
-            query.{" "}
-            <span>
-              (N.B.: If you have a general question that you think might benefit
-              others in the group, always consider posting in an appropriate
-              channel before sending a DM to an admin.)
-            </span>
-          </li>
+          {CoC_Contents.UNACCEPTABLE_BEHAVIOR.map((list) => {
+            return (
+              <li key={list.id}>
+                {list.id}. {list.text}
+              </li>
+            );
+          })}
           <p>*Taken from Toronto Javascript</p>
 
           <h5>Examples of Unaccepted Behaviors:</h5>
           <ul>
-            <li>Unsolicited sexual advances</li>
-            <li>Trolling, insulting, derogatory comments </li>
-            <li>Public or private harassment </li>
-            <li>
-              Leaking private information such as email address or physical
-              address{" "}
-            </li>
-            <li>Any conduct you would not do in a professional setting</li>
+            {CoC_Contents.EXAMPLES_UNACCEPTABLES.map((item) => {
+              return <li key={item}>{item}</li>;
+            })}
           </ul>
         </ul>
       </div>
@@ -116,56 +51,28 @@ export default function CoC() {
       <div>
         <h4>Acceptable Behaviors</h4>
         <ul>
-          <li>
-            A. Fostering empathy and kindness. Treating others the way you would
-            want to be treated.
-          </li>
-          <li>
-            B. Respecting each other’s differing opinions, viewpoints and
-            experiences
-          </li>
-          <li>C. Giving constructive feedback</li>
-          <li>
-            D. Focusing on what is not best for us as individuals, but for the
-            overall community
-          </li>
-          <li>
-            E. Fostering empathy and kindness. Treating others the way you would
-            want to be treated.
-          </li>
-          <li>F. Most importantly, having fun and creating a safe community</li>
+          {CoC_Contents.EXAMPLES_ACCEPTABLES.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
         </ul>
       </div>
       {/* REFERRAL POLICY */}
       <div>
         <h4>Referral Policy</h4>
-        <p>
-          Recognizing the importance of referrals in professional networking,
-          our administrative team has identified the need for explicit
-          guidelines to foster beneficial interactions and maintain the
-          integrity of our organization.
-        </p>
+        <p>{CoC_Contents.REFERRAL_POLICY_TEXT}</p>
         <ul>
-          <li>
-            1. Referral Request Prudence: Members should refrain from requesting
-            referrals during their initial interaction with an individual.
-            It&apos;s essential to remember that successful referrals depend on
-            a relationship of trust and mutual understanding, which requires
-            more than a single interaction to establish.
-          </li>
-          <li>
-            Respect for Professional Reputation: We must acknowledge that when
-            someone provides a referral, they are staking their professional
-            reputation on the line. Consequently, requesting a referral should
-            not be a casual inquiry but a mindful consideration of the potential
-            implications for the referring party.
-          </li>
+          {CoC_Contents.REFERRAL_POLICIES.map((list) => {
+            return (
+              <li key={list.id}>
+                <span>
+                  {list.id}. {list.title}:
+                </span>{" "}
+                {list.text}
+              </li>
+            );
+          })}
         </ul>
-        <p>
-          By adhering to these guidelines, we can maintain a respectful,
-          professional environment that fosters genuine networking and promotes
-          sustainable growth for all members.
-        </p>
+        <p>{CoC_Contents.REFERRAL_POLICY_CONCL}</p>
       </div>
     </section>
   );
---
+++ utils/constants.tsx
@@ -0,0 +1,121 @@
+interface Enforcement {
+  id: number;
+  title: string;
+  text: string;
+}
+
+interface Unacceptable {
+  id: string;
+  text: string;
+}
+
+interface Referrals {
+  id: number;
+  title: string;
+  text: string;
+}
+
+interface PageConstants {
+  SCOPE_TEXT: string;
+  ENFORCEMENT_TEXT: string;
+  ENFORCEMENT_LIST: Enforcement[];
+  UNACCEPTABLE_BEHAVIOR: Unacceptable[];
+  EXAMPLES_UNACCEPTABLES: Array<string>;
+  EXAMPLES_ACCEPTABLES: Array<string>;
+  REFERRAL_POLICY_TEXT: string;
+  REFERRAL_POLICIES: Referrals[];
+  REFERRAL_POLICY_CONCL: string;
+}
+
+export const CoC_Contents: PageConstants = {
+  SCOPE_TEXT:
+    "We, the admin team at TechTank, are happy to have grown to such a large group in such a short amount of time. As we value the participation and enjoyment of everyone in our community, we have decided to release community guidelines/codes of conduct. Our goal and wish is that everyone has a positive experience, and these codes apply to ALL community spaces: our slack workspace, in-person and virtual social gatherings, and direct messages. Repeated unacceptable behaviour will lead to expulsion from the group as we are here to create a safe and inclusive space for everyone.",
+  ENFORCEMENT_TEXT:
+    "Instances of abuse, harassment or any unacceptable behavior can be reported to Chris, Erik, Nonso, and Sammy. We will take corrective action in response to any behavior deemed inappropriate, threatening, offensive or harmful. We have a right to protect this space and individuals within.",
+  ENFORCEMENT_LIST: [
+    {
+      id: 1,
+      title: "Notice of Correction",
+      text: "We will contact said individual to correct their behavior via slack.",
+    },
+    {
+      id: 2,
+      title: "Temporary Ban",
+      text: "A ban from the community for 2 weeks. This includes interaction in community spaces, external channels and direct messages on and off the community.",
+    },
+    {
+      id: 3,
+      title: "Permanent Ban",
+      text: "Self-explanatory.",
+    },
+  ],
+  UNACCEPTABLE_BEHAVIOR: [
+    {
+      id: "A",
+      text: "No hate speech or discriminatory language: Do not engage in or tolerate any form of hate speech, including but not limited to racist, sexist, homophobic, transphobic, or ableist language.",
+    },
+    {
+      id: "B",
+      text: "No discrimination: Treat all individuals with fairness, respect, and dignity, regardless of their race, gender, sexual orientation, religion, nationality, disability, or any other characteristic..",
+    },
+    {
+      id: "C",
+      text: "No harassment: Do not engage in any form of harassment, including verbal, written, physical, or visual harassment. This includes but is not limited to offensive jokes, derogatory comments, unwelcome advances, or persistent unwelcome communication. This includes asking for referrals from people you have not had contact with prior.",
+    },
+    {
+      id: "D",
+      text: "No bullying or intimidation: Do not engage in any behavior intended to intimidate, belittle, or demean others within the group.",
+    },
+    {
+      id: "E",
+      text: "*No feigning surprise: Don't act surprised or belittle others when they express not knowing something, whether it's a technical or non-technical topic.",
+    },
+    {
+      id: "F",
+      text: "*No well-actually's: Avoid making minor corrections solely for the purpose of grandstanding, particularly if they don't significantly contribute to the conversation.",
+    },
+    {
+      id: "G",
+      text: "*No back-seat driving (in irl, face-to-face contexts): Refrain from intermittently offering unsolicited advice or interrupting ongoing conversations, as it can be disruptive and disrespectful. Instead, fully engage and participate when helping or joining discussions.",
+    },
+    {
+      id: "H",
+      text: "Respect other people’s boundaries. No means no.",
+    },
+    {
+      id: "I",
+      text: "Respect other people’s time. Many folx in the group, including the admins, have personal and professional lives outside of the group. Expect delays in responses especially on weekends and weeknights, and understand it might take time to respond to your query. (N.B.: If you have a general question that you think might benefit others in the group, always consider posting in an appropriate channel before sending a DM to an admin.)",
+    },
+  ],
+  EXAMPLES_UNACCEPTABLES: [
+    "Unsolicited sexual advances",
+    "Trolling, insulting, derogatory comments",
+    "Public or private harassment",
+    "Leaking private information such as email address or physical address",
+    "Any conduct you would not do in a professional setting",
+  ],
+  EXAMPLES_ACCEPTABLES: [
+    "Fostering empathy and kindness. Treating others the way you would want to be treated.",
+    "Respecting each other’s differing opinions, viewpoints and experiences",
+    "Giving constructive feedback",
+    "Focusing on what is not best for us as individuals, but for the overall community",
+    "Fostering empathy and kindness. Treating others the way you would want to be treated.",
+    "Most importantly, having fun and creating a safe community",
+  ],
+  REFERRAL_POLICY_TEXT:
+    "Recognizing the importance of referrals in professional networking, our administrative team has identified the need for explicit guidelines to foster beneficial interactions and maintain the integrity of our organization.",
+  REFERRAL_POLICIES: [
+    {
+      id: 1,
+      title: "Referral Request Prudence",
+      text: "Members should refrain from requesting referrals during their initial interaction with an individual. It's essential to remember that successful referrals depend on a relationship of trust and mutual understanding, which requires more than a single interaction to establish.",
+    },
+    {
+      id: 2,
+      title: "Respect for Professional Reputation",
+      text: "We must acknowledge that when someone provides a referral, they are staking their professional reputation on the line. Consequently, requesting a referral should not be a casual inquiry but a mindful consideration of the potential implications for the referring party.",
+    },
+  ],
+  REFERRAL_POLICY_CONCL:
+    "By adhering to these guidelines, we can maintain a respectful, professional environment that fosters genuine networking and promotes sustainable growth for all members.",
+};
