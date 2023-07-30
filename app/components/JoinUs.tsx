bc4312e1eed03a844e9f2b694cf3534634baeda4
dreymotoh <striclyspoken@gmail.com>
2023-07-30 14:00:44 -0400

    made changes to styling using prettier, camel case for base css class, removed a few elements that were commented out

--- app/components/JoinUs.tsx
+++ app/components/JoinUs.tsx
@@ -1,63 +1,73 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Slack from "../../public/slack.png"
-import Meetup from "../../public/Meetup_Logo.png"
-import Github from "../../public/GitHub_Invertocat_Logo.svg"
+import Slack from "../../public/slack.png";
+import Meetup from "../../public/Meetup_Logo.png";
+import Github from "../../public/GitHub_Invertocat_Logo.svg";
 
 const JoinUs = React.forwardRef<HTMLElement, {}>((props, ref) => {
   return (
     <div className="h-[600px] max-w-[1440px]">
-    {/* <section ref={ref}> */}
       <div className="text-center">
         <h1 className="text-4xl">Join Us!</h1>
       </div>
 
       <div className="flex flex-col">
         <div className="flex flex-wrap justify-center gap-[100px] mt-10 ml-13">
-          <div className="card--format"> 
+          <div className="cardFormat">
             <div className="flex justify-center align-center text-center">
-              <Link target="_blank" href="https://join.slack.com/t/thetechtank/shared_invite/zt-1y9l2ste9-ICirf8fhVqJtW_pHfwA_yQ" className="flex flex-col items-center justify-center">
-                 <Image src={Slack} alt="slack icon" width={200} height={200} />
-               <h3 className="text-2xl">Slack</h3>
-              <p className="w-[80%] text-center mt-5 text-sm">
-                Join us on our Slack channel where you can learn more about
-                networking and social events, and learn/grow together!
-              </p>
-            </Link>
+              <Link
+                target="_blank"
+                href="https://join.slack.com/t/thetechtank/shared_invite/zt-1y9l2ste9-ICirf8fhVqJtW_pHfwA_yQ"
+                className="flex flex-col items-center justify-center"
+              >
+                <Image src={Slack} alt="slack icon" width={200} height={200} />
+                <h3 className="text-2xl">Slack</h3>
+                <p className="w-[80%] text-center mt-5 text-sm">
+                  Join us on our Slack channel where you can learn more about
+                  networking and social events, and learn/grow together!
+                </p>
+              </Link>
             </div>
           </div>
 
-          <div className="card--format">
+          <div className="cardFormat">
             <div className="flex justify-center align-center text-center">
-              <Link target="_blank" href="https://www.meetup.com" className="flex flex-col items-center justify-center mt-[18px]">
-                 <Image src={Meetup} alt="slack icon" width={200} height={200}/>
-               <h3 className="text-2xl">Meetup</h3>
-               <p className="w-[80%] text-center mt-6 text-sm">
-                Join us on Meetup for in-person events and networking sessions
-                based in Toronto!
-               </p>
-             </Link>
+              <Link
+                target="_blank"
+                href="https://www.meetup.com"
+                className="flex flex-col items-center justify-center mt-[18px]"
+              >
+                <Image src={Meetup} alt="slack icon" width={200} height={200} />
+                <h3 className="text-2xl">Meetup</h3>
+                <p className="w-[80%] text-center mt-6 text-sm">
+                  Join us on Meetup for in-person events and networking sessions
+                  based in Toronto!
+                </p>
+              </Link>
             </div>
           </div>
 
-          <div className="card--format">
+          <div className="cardFormat">
             <div className="flex justify-center align-center text-cente">
-              <Link target="_blank" href="https://github.com/tech-tankorg" className="flex flex-col items-center justify-center mt-[18px]">
-                 <Image src={Github} alt="slack icon" width={200} height={200}/>
+              <Link
+                target="_blank"
+                href="https://github.com/tech-tankorg"
+                className="flex flex-col items-center justify-center mt-[18px]"
+              >
+                <Image src={Github} alt="slack icon" width={200} height={200} />
                 <h3 className="text-2xl">Github</h3>
                 <p className="w-[80%] text-center mt-5 text-sm">
-                Join our Github organization to contribute towards our
-                open-source community and build together!
+                  Join our Github organization to contribute towards our
+                  open-source community and build together!
                 </p>
               </Link>
             </div>
           </div>
         </div>
       </div>
-    {/* </section> */}
     </div>
   );
 });
