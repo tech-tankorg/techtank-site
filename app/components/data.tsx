c43056a0cbd9418a359725bbedec084cbb773aee
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2026-03-05 15:48:34 -0500

    feat(events): add events data layer and map to page components

---
+++ app/components/data.tsx
@@ -0,0 +1,9 @@
+export const slackInvite =
+  "https://join.slack.com/t/thetechtank/shared_invite/zt-2oou5qbue-LXNB4M7~C_6CBAImj1kpJA";
+
+export const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Sponsor", href: "/sponsor" },
+  { label: "Events", href: "/events" },
+];
